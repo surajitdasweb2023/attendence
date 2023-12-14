4643c26e9eda1c1547deb19e0ba7344eb282de57
surajit <surajit.das@findmore.pt>
2023-12-14 13:26:14 +0530

    Structure has been added only api has to be added

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-const randtoken = require("rand-token");
-const passport = require("passport");
-const nodemailer = require("nodemailer");
-
-const express = require("express");
-const multer = require("multer");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const multer = require("multer");
+const randtoken = require("rand-token");
 const uuid = require("uuid").v4;
-require("dotenv").config();
-let lastUUID = "";
-
-// DB Operations
-const dbooperation = require('./dboperations');
 
 const app = express();
-const refreshTokens = {};
-
-const passportOpts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.PASSPORT_SECRET,
-};
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+const refreshTokens = {};
+let lastUUID = "";
+
+
+// CORS ORIGIN CONFIGURATIONS
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.ALLOW_ORIGINS);
   res.header(
@@ -47,6 +40,13 @@ app.use(function (req, res, next) {
   }
 });
 
+
+
+// PASSPORT CONFIGURATIONS
+const passportOpts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.PASSPORT_SECRET,
+};
 passport.use(
   new JwtStrategy(passportOpts, function (jwtPayload, done) {
     const expirationDate = new Date(jwtPayload.exp * 1000);
@@ -56,38 +56,43 @@ passport.use(
     done(null, jwtPayload);
   })
 );
-
 passport.serializeUser(function (user, done) {
   done(null, user.username);
 });
 
+
+
+// DB Operations
+const dbooperation = require('./dboperations');
+
+
 // Setting to user public folder as public folder (default value : index.html)
 app.use(express.static('public'));
 
+
+
+// UTILITY FUNCTIONS
 function mysqlResults(result) {
   return JSON.parse(JSON.stringify(result));
 }
-
-// File upload logic if any
-const fileNameSeperator = "_";
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/uploads");
-  },
-  filename: (req, file, cb) => {
-    const { originalname } = file;
-    lastUUID = uuid();
-    cb(null, lastUUID + fileNameSeperator + originalname);
-  },
-});
-const upload = multer({ storage: storage });
-
 function generateAccessToken(user) {
   return jwt.sign({ email: user.email }, process.env.PASSPORT_SECRET, {
     expiresIn: process.env.TOKEN_EXPIRY_IN_SECS,
   });
 }
+function authenticateTokenForAdmin(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401)
 
+  jwt.verify(token, process.env.PASSPORT_SECRET, (err, user) => {
+    if (user.userType !== 'admin') return res.sendStatus(401);
+    if (err) return res.sendStatus(403)
+
+    req.user = user;
+    next();
+  })
+}
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["Authorization"];
   const token = authHeader && authHeader.split(" ")[1];
@@ -100,7 +105,6 @@ function authenticateToken(req, res, next) {
     next();
   });
 }
-
 async function sendMail(email, subject, htmlTemplate) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -123,7 +127,35 @@ async function sendMail(email, subject, htmlTemplate) {
     return error;
   }
 }
+function generateRandomPassword(length) {
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var password = "";
 
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+
+
+// UPLOADING FILES INTO THE SYSTEM
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    const { originalname } = file;
+    lastUUID = uuid();
+    cb(null, lastUUID + "_" + originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+
+// APIs to invoke from Web Application
 app.get('/getAllUsers', (req, res) => {
   dbooperation.getAllUsers().then(result => {
     const data = mysqlResults(result);
@@ -154,6 +186,8 @@ app.get('/getAllUsers', (req, res) => {
   })
 });
 
+
+// SERVER START WITH PORT
 const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
